1459c9349c867188532b160df9cb409ea266e99e
Crows19 <crows19@example.com>
2025-05-05 21:40:37 -0500

    I am doing my homework.

--- ece103_hw11_2025/external_data.cpp
+++ ece103_hw11_2025/external_data.cpp
@@ -4,16 +4,12 @@
 #include "external_data.h"
 
 char nickname_list[25][50] = {
-    "Dr. Mali*", 
+    "Dr. Mali*",
+    "Crows19", 
 //*** I suggest that you do not modify above ***
 
 
 
 //*** I suggest that you do not modify below ***
     "" // the last entry and all potential remaining entires are empty strings
-};
-
-char nickname[25][50] = {
-    "HyrdroMouse",
-   
 };
