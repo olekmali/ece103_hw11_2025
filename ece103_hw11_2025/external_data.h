3fccf099a800f924fc4e6c788e6acd2007466fb4
Aleksander Malinowski <olekmali@bradley.edu>
2025-05-09 21:22:51 -0500

    Almost done.

--- ece103_hw11_2025/external_data.h
+++ ece103_hw11_2025/external_data.h
@@ -6,8 +6,8 @@
 #ifndef _external_data_h
 #define _external_data_h
 
-constexpr char homework_version[] = "3.16";
-// This is the version of the homework project was last time updated on May 8, 2025
+constexpr char homework_version[] = "3.17";
+// This is the version of the homework project was last time updated on May 9, 2025
 
 // The instrctor will do his best to update this file every day until the assignment is due.
 
